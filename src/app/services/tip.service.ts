00dc6b3ee6974362508875674a6d08bf0cdd2a6c
Andrea Civita <andreacivita994@gmail.com>
2018-11-30 07:18:55 +0100

    fix http

--- src/app/services/tip.service.ts
+++ src/app/services/tip.service.ts
@@ -7,7 +7,7 @@ import {HttpClient} from '@angular/common/http';
 export class TipService {
 
   public headers;
-  private baseUrl = 'https://90.147.188.53/backend/api/';
+  private baseUrl = 'http://90.147.188.53/backend/api/';
   private tokenUrl = '';
 
   constructor(private http: HttpClient) {
