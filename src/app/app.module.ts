6d2197807970c2a72306786c153834bce34bf20c
Andrea Civita <andreacivita994@gmail.com>
2018-11-30 02:56:15 +0100

    Tutti i component (per ora)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { AppComponent } from './app.component';
 import { BlogcomponentComponent } from './blogcomponent/blogcomponent.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProdottiComponent } from './prodotti/prodotti.component';
+import { ProdottoComponent } from './prodotto/prodotto.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { TipComponent } from './tip/tip.component';
+import { BlogComponent } from './blog/blog.component';
 
 const routes: Routes = [
   {
@@ -22,7 +34,19 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     BlogcomponentComponent,
-    HomeComponentComponent
+    HomeComponentComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ShopComponent,
+    ProdottiComponent,
+    ProdottoComponent,
+    CategoriaComponent,
+    CategorieComponent,
+    LoginComponent,
+    CartComponent,
+    TipComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
